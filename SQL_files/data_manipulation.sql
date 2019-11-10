17309dfdbd3d3a72a00ef9ab24fe8b30c36416cb
AidenJN <nelsonai@oregonstate.edu>
2019-11-10 13:53:24 -0800

    Added some skeletons

---
+++ SQL_files/data_manipulation.sql
@@ -0,0 +1,27 @@
+-- ** DATA MANIPULATION QUERIES **
+-- This file contains the queries that will be triggered
+-- by data oriented interactions with the CanVast web
+-- application.
+
+-- Searching for Painting
+
+
+-- Transfering Art
+
+
+-- Deleting Art
+
+
+-- Add new Painting
+
+
+-- Add new Artist
+
+
+-- Add new Gallery
+
+
+-- Add new Category
+
+
+-- Add new Medium
